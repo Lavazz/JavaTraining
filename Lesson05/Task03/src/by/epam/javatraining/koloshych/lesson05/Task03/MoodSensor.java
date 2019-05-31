168e8dc3a7dfb68e0241b9d8578018458060f3c9
Katsiaryna <koloshych@gmail.com>
2019-05-31 22:24:53 +0300

    change Random

--- Lesson05/Task03/src/by/epam/javatraining/koloshych/lesson05/Task03/MoodSensor.java
+++ Lesson05/Task03/src/by/epam/javatraining/koloshych/lesson05/Task03/MoodSensor.java
@@ -4,11 +4,8 @@ import java.util.Random;
 
 public class MoodSensor {
     public static void main(String[] args) {
-        int min = 0;
-        int max = 10;
-        int diff = max - min;
         Random random = new Random();
-        int ratingMood = random.nextInt(diff + 1);
+        int ratingMood = random.nextInt(11);
         char mood = MoodSensorLogic.identifyMood(ratingMood);
         View.print(mood);
     }
